1c7d5808e863b017e3813b8a069b54001e8c4ccc
Tensae Mamo <tensaemamo@protonmail.com>
2024-01-22 14:47:14 +0100

    impl using bootstrap

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,16 @@
 class QuestionsController < ApplicationController
+  def ask
+    # @question = params[:question]
+  end
+
+  def answer
+    @question = params[:question]
+    if @question == "hello"
+      @answer = "hi!";
+    elsif @question == "what time is it?"
+      @answer = Time.now;
+    else
+      @answer = "TODO";
+    end
+  end
 end
